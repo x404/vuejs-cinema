b14a0c4872f1ca47f7bf023ac905512b7feb682b
x404 <kronos2003@gmail.com>
2017-07-02 14:29:17 +0300

    Создание слотов

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,12 +1,16 @@
 <template>
 	<div id="movie-list">
 		<div v-if="filterMovies.length">
-			<movie-item v-for="movie in filterMovies" 
-				v-bind:movie="movie.movie" 
-				v-bind:sessions="movie.sessions"
-				v-bind:day="day"
-				v-bind:time="time"
-			></movie-item>
+			<movie-item v-for="movie in filterMovies" v-bind:movie="movie.movie">
+				<div class="movie-sessions">
+					<div v-for="session in filteredSessions(movie.sessions)" class="session-time-wrapper">
+						<div class="session-time">
+							{{ formatSessionTime(session.time)  }}
+						</div>
+					</div>
+				</div>
+
+			</movie-item>
 		</div>
 		<div v-else-if="movies.length" class="no-results">
 			{{ noResults }}
@@ -26,6 +30,12 @@
 	export default {
 			props: [ 'genre', 'time' , 'movies', 'day'],
 			methods: {
+				formatSessionTime(raw){
+					return this.$moment(raw).format('h:mm A')
+				},
+				filteredSessions(sessions){
+					return sessions.filter(this.sessionPassesTimeFilter);
+				},
 				moviePassesGenreFilter(movie){
 					if (!this.genre.length){
 						return true;
