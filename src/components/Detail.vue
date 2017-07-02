834134883801404a838719c258d2efae4d619e72
x404 <kronos2003@gmail.com>
2017-07-02 14:12:25 +0300

    105 - Показ фильма на отдельной странице

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,6 +1,21 @@
 <template>
-	<div>Hello world!</div>
+	<div id="detail" v-if="movie">
+		<movie-item v-bind:movie="movie.movie" v-bind:sessions="[]"></movie-item>
+	</div>
 </template>
 <script>
-	
+	import MovieItem from './MovieItem.vue';
+
+	export default{
+		props: [ 'movies' ],
+		computed: {
+			movie(){
+				let movie = this.movies.find(movie => movie.id === this.$route.params.id);
+				return movie ? movie : null;
+			}
+		},
+		components: {
+			MovieItem
+		}
+	}	
 </script>
